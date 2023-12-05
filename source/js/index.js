ad2655be1cb4fcfa765529fdc24a43359e4d2b64
thely314 <146166116+thely314@users.noreply.github.com>
2023-12-05 13:46:26 +0800

    try audioPlayer

--- source/js/index.js
+++ source/js/index.js
@@ -1,6 +1,5 @@
 var song_list=["果てなき風の軌跡さえ~破~","琪露诺的完美算术教室","琪露诺的完美算数教室⑨周年版","NEEDY GIRL OVERDOSE","INTERNET YAMERO","二人の魔法","Sweet Treasure (Inst.)"];
 var i = 0;
-var magicStatus = 0;
 
 var audioPlayer = document.getElementById("audioPlayer");
 
@@ -8,32 +7,30 @@ var audioPlayer = document.getElementById("audioPlayer");
 window.onload = function() {
     var audio = document.getElementById("audioPlayer");
     audio.volume = 0.2;
-    var magicSound = document.getElementById("magicSound");
-    magicSound.volume = 0.2;
 };
 
 function play() {
     if (audioPlayer.paused) {
         audioPlayer.play();
-        document.getElementById('musicImg').src="img/pauseButton.png";
+        document.getElementById('musicImg').src="source/img/pauseButton.png";
     }else{
         audioPlayer.pause();
-        document.getElementById('musicImg').src="img/startButton.png";
+        document.getElementById('musicImg').src="source/img/startButton.png";
     }
 }
 
 function end() {
   audioPlayer.pause();
-  document.getElementById('musicImg').src="img/startButton.png";
+  document.getElementById('musicImg').src="source/img/startButton.png";
   audioPlayer.currentTime = 0;//让音乐从头播放
 }
 
 function change(n){
     i=n;
-    audioPlayer.src = "music/"+song_list[i]+".mp3";
+    audioPlayer.src = "source/music/"+song_list[i]+".mp3";
     audioPlayer.load();
     audioPlayer.play();
-    document.getElementById('musicImg').src="img/pauseButton.png";
+    document.getElementById('musicImg').src="source/img/pauseButton.png";
     changeShowedText(i);
 }
 
@@ -45,10 +42,10 @@ function next(){
     else{
        i=0;
     }
-    audioPlayer.src = "music/"+song_list[i]+".mp3";
+    audioPlayer.src = "source/music/"+song_list[i]+".mp3";
     audioPlayer.load();
     audioPlayer.play();
-    document.getElementById('musicImg').src="img/pauseButton.png";
+    document.getElementById('musicImg').src="source/img/pauseButton.png";
     changeShowedText(i);
 }
 
@@ -63,20 +60,4 @@ function changeVolume(value) {
 function changeShowedText(i){
     var showedText = document.getElementById("currentMusic");
     showedText.innerText = "当前播放: " + song_list[i];
-}
-
-function magicSpell(){
-    var magicSound = document.getElementById("magicSound");
-    if(magicStatus == 0){
-        magicSound.src = "music/magicSpell_emotionTest.wav";
-        magicSound.load();
-        magicSound.play();
-        magicStatus = 1;
-    }
-    else{
-        magicSound.src = "music/magicSpell.wav";
-        magicSound.load();
-        magicSound.play();
-        magicStatus = 0;
-    }
 }
