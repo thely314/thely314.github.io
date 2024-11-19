2383b0b3a7d4c8c6bc71ec30e06faf584898dbf2
lone <1715849169@qq.com>
2024-11-19 23:06:54 +0800

    Site updated: 2024-11-19 23:06:52

--- js/pikaday.js
+++ js/pikaday.js
@@ -22,7 +22,7 @@
       return [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
     },
     Pikaday = function (opts) {
-      var data = '{"2023":{"11":{"5":true,"9":true}},"2024":{"10":{"10":true,"11":true}}}';
+      var data = '{"2023":{"11":{"5":true,"9":true}},"2024":{"10":{"10":true,"11":true,"19":true}}}';
       this.data = JSON.parse(data);
       window.pdata = this.data;
       this.weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
